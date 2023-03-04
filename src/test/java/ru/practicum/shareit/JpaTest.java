8f66d80a4d3622e69d4c987611c477a311a8d531
SergeiA <as.antropov@yandex.ru>
2023-03-04 13:27:56 +0300

    <fix>: <Для тестов на гите>

--- src/test/java/ru/practicum/shareit/JpaTest.java
+++ src/test/java/ru/practicum/shareit/JpaTest.java
@@ -2,12 +2,11 @@ package ru.practicum.shareit;
 
 import org.springframework.boot.test.autoconfigure.orm.jpa.AutoConfigureDataJpa;
 import org.springframework.boot.test.context.SpringBootTest;
-import ru.practicum.shareit.config.DbConfig;
 import ru.practicum.shareit.item.service.impl.ItemServiceImpl;
 import ru.practicum.shareit.user.service.impl.UserServiceImpl;
 
 @AutoConfigureDataJpa
-@SpringBootTest(classes = {DbConfig.class, UserServiceImpl.class, ItemServiceImpl.class},
+@SpringBootTest(classes = {UserServiceImpl.class, ItemServiceImpl.class},
         properties = {
                 "spring.jpa.properties.hibernate.format_sql=true",
                 "spring.jpa.show-sql=true",
