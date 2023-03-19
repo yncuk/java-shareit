c9d0ea73bc41bd14580077ce21f281e861053ad1
SergeiA <as.antropov@yandex.ru>
2023-03-20 00:54:07 +0300

    <fix>: <Для тестов>

--- src/main/java/ru/practicum/shareit/exceptions/ConflictException.java
+++
@@ -1,7 +0,0 @@
-package ru.practicum.shareit.exceptions;
-
-public class ConflictException extends RuntimeException {
-    public ConflictException(final String message) {
-        super(message);
-    }
-}
--- src/main/java/ru/practicum/shareit/exceptions/EntityNotFoundException.java
+++
@@ -1,7 +0,0 @@
-package ru.practicum.shareit.exceptions;
-
-public class EntityNotFoundException extends RuntimeException {
-    public EntityNotFoundException(final String message) {
-        super(message);
-    }
-}
